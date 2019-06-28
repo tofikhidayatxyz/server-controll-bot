a49bd870061c5f2b6afdf7a76e915cd26d7d1393
Tofikhidayat <topikhidayat711@gmail.com>
2019-06-29 01:16:49 +0700

    server controll

---
+++ index.js
@@ -0,0 +1,105 @@
+"use strict";
+
+const Slimbot = require('slimbot');
+const token = '771213330:AAFRE4O6sD_mjD1j6YR2lm2qZEtrzMGXixs';
+const path  =  require("path");
+const env  =  require("dotenv").config();
+const git = require('simple-git/promise');
+const command  =  require("./command.json");
+const { exec , spawn }   = require('child_process');
+const adminId =  process.env.ADMIN_ID;
+const bot = new Slimbot(process.env.TOKEN);
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  ///  run 
+  if (msg.text.substring(0,3).toLowerCase() == "run") {
+		let tmp =  msg.text.split("@");
+		let dir  =  tmp[1].split(" ").join("")
+		let cwd  =  tmp[2];
+		for (var i = 0; i < command[0]["list"].length; i++) {
+			if (dir == command[0]["list"][i].name) {
+				dir  =  path.join(__dirname,command[0]["list"][i]["path"]);
+				let main  = exec(`cd  ${dir}  &&  ${cwd}`); 
+					main.stdout.on('data', (data) => {
+					  bot.sendMessage(adminId,"Response : \n"+ data);
+					  if (chatId != adminId) {
+					  	bot.sendMessage(chatId,"Response : \n"+ data);
+					  }
+					});
+					main.stderr.on('data', (data) => {
+					  bot.sendMessage(adminId, "Error : \n" +data);
+					  if (chatId != adminId) {
+					  	bot.sendMessage(chatId,"Error : \n"+ data);
+					  }
+					});
+					main.on('close', (code) => {
+					  bot.sendMessage(adminId, `Closed ${code}`);
+					  if (chatId != adminId) {
+					  	bot.sendMessage(chatId,`Closed ${code}`);
+					  }
+					});
+					//main.kill('SIGHUP');
+			}
+		}
+	}
+
+	// git
+	if (msg.text.substring(0,3).toLowerCase() == "git") { 
+		let tmp =  msg.text.split("@");
+		let dir  =  tmp[1].split(" ").join("")
+		let cwd  =  tmp[2].split(" ").join("");
+		let args  =  tmp[3];
+		let remote;
+		for (var i = 0; i < command[1]["list"].length; i++) {
+			let config  = command[1]["list"][i];
+			if (config["name"] == dir) {
+				dir  =  path.join(__dirname,config["path"]);
+				remote  =  `https://${process.env.GIT_USER}:${process.env.GIT_PASS}@${config.repo}`;
+				if (cwd == "pull") {
+					git(dir).pull(remote, "master", "--force" ).then((stat)=> {
+						let push  = `         Files : ${JSON.stringify(stat.files)}
+									 Insertions : ${JSON.stringify(stat.insertions)}
+									 Deletions : ${JSON.stringify(stat.deletions)}
+									 Summary : ${JSON.stringify(stat.summary)}  
+									 Created : ${JSON.stringify(stat.created)}  
+									 Deleted : ${JSON.stringify(stat.deleted)}  
+									`;
+						 bot.sendMessage(adminId, `Response :\n ${push}`);
+						  if (chatId != adminId) {
+						  	bot.sendMessage(chatId,`Response :\n ${push}`);
+						  }
+					}).catch((e)=>{
+						 bot.sendMessage(adminId, `Error : \n${e}`);
+						  if (chatId != adminId) {
+						  	bot.sendMessage(chatId,`Error : \n${e}`);
+						  }
+					})
+				}
+			}
+		}
+	}
+	else {
+		bot.sendMessage(chatId,`Opps Command notfound`);
+	}
+
+
+});
+
+bot.startPolling();
+
+/*
+const user = 'tofikhidayat';
+const pass = 'mazenda9999';
+const dir = path.join(__dirname,"../smk");
+const repo =  "github.com/tofikhidayat/smk";
+const remote  =  `https://${user}:${pass}@${repo}`;
+
+git(dir).pull(remote, "master", "--force" ).then((err)=> {
+	console.log(err);
+}).catch((e)=>{
+	console.lcog(e)
+})*/
+
+//console.log(process.env.GIT_USER)
